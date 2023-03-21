c9d7c1e806cfcaed8001b4de41140064822d5737
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-03-21 17:06:09 +0700

    menambahkan class SimpleDataBase

--- modul5_1302213037/Program.cs
+++ modul5_1302213037/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.ComponentModel;
+
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates;
+
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+        inputDates = new List<DateTime>();
+    }
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data" + (i + 1) + " berisi: " + storedData[i] + " yang disimpan pada waktu " + inputDates[i]);
+        }
+
+
+    }
+}
+
+public class Program
+    {
+   public static void Main(string[] args)
+   {
+     SimpleDataBase<int> database = new SimpleDataBase<int>();
+     database.AddNewData(13);
+     database.AddNewData(22);
+     database.AddNewData(37);
+     database.PrintAllData();
+   }
+}
+
+
+
