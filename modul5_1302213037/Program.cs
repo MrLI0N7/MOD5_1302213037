4de9a20c289d3de8837ef95bb7923c63e8c57d85
jaatsiyah <jaatsiyah@student.telkomuniversity.ac.id>
2023-03-21 16:22:23 +0700

    implementasi-generic-method
    menambahkan class Penjumlahan dengan method
    JumlahTigaAngka

--- modul5_1302213037/Program.cs
+++ modul5_1302213037/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class Penjumlahan<T>
+{
+    public void JumlahTigAngka(T var1, T var2, T var3){
+
+        int sum = (dynamic)var1 + var2 + var3;
+        Console.WriteLine(sum);
+    }
+
+
+} 
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Penjumlahan<int> angka = new Penjumlahan<int>();
+
+        int input1 = 13 ;
+        int input2 = 22 ;
+        int input3 = 37;
+        
+        angka.JumlahTigAngka(input1, input2, input3);
+        
+
+    }
+}
